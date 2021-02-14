86bc63b1e1bb9849bd4704b59234b9a964fe6212
fuxinliang <fuxinliang@zuoyebang.com>
2021-02-14 23:19:45 +0800

    新增dfs和bfs遍历方法

--- graph/ArrayGraph.cpp
+++ graph/ArrayGraph.cpp
@@ -4,12 +4,17 @@
 
 #define MaxNum 10  //顶点数的最大值
 
+#define  MaxSize 10
+
+
 //邻接矩阵
 typedef struct Arc {
     char Vex[MaxNum];  //存储顶点信息
     int Edge[MaxNum][MaxNum];  //边上权值的数据类型
     int vexnum,edgenum; //顶点数,边数
 }MGragh,*Gragh;
+
+
 
 /**
  * @desc 返回当前节点在顶点集合中的下标
@@ -96,8 +101,120 @@ void CreateByArray(Gragh &G) {
 
 }
 
+/**
+ * 获取顶点v的第一个邻接顶点的索引,失败则返回-1
+ * @param G
+ * @param v
+ * @return
+ */
+int FirstVertex(Gragh G, int v) {
+
+    if(v < 0 || v > G->vexnum -1) {
+        return  -1;
+    }
+
+    for (int j = 0; j < G->vexnum; ++j) {
+        if(G->Edge[v][j] == 1) {
+            return j;
+        }
+    }
+    return -1;
+}
+
+/**
+ * 返回顶点v相对于w的下一个邻接顶点的索引，失败则返回-1
+ * @param G
+ * @param v
+ * @param w
+ * @return
+ */
+int NextVertex(Gragh G, int v, int w) {
+
+    int i;
+
+    if (v<0 || v>(G->vexnum-1) || w<0 || w>(G->vexnum-1))
+        return -1;
+
+    for (i = w + 1; i < G->vexnum; i++)
+        if (G->Edge[v][i] == 1)
+            return i;
+
+    return -1;
+
+}
 
 
+/**
+ * DFS 广度优先遍历-类似于图的层次遍历
+ * @param G
+ */
+void BFS(Gragh G) {
+
+    int head = 0;
+    int rear = 0;
+    int queue[MaxSize];     // 辅组队列
+    int visited[MaxSize];   // 顶点访问标记
+    int i, j, k;
+
+    for (i = 0; i < G->vexnum; i++)
+        visited[i] = 0;
+
+    printf("BFS遍历依次为: ");
+    for ( i = 0; i < G->vexnum; i++) {
+        if(!visited[i]) {
+            visited[i] = 1;
+            printf("%c ",G->Vex[i]);
+            queue[rear++] = i; //入队列
+        }
+
+        while (head != rear) { //队列不空
+
+            j = queue[head++]; //出队列
+            for (k=FirstVertex(G,j); k >= 0; k = NextVertex(G,j,k)) {
+                if (!visited[k])
+                {
+                    visited[k] = 1;
+                    printf("%c ", G->Vex[k]);
+                    queue[rear++] = k;
+                }
+            }
+
+        }
+    }
+    printf("\n");
+
+}
+
+
+int visited[MaxSize];
+void DFS(Gragh G, char ch) {
+
+    int index;
+
+    index = LocateVex(G,ch); //获取该节点的存储下标 
+    visited[index] = 1; //标记为已访问
+    
+    printf("%c ",ch);
+    for (int i = 0; i < G->vexnum; i++) {
+        if(!visited[i]) {
+            DFS(G,G->Vex[i]);
+        }
+    }
+    
+
+}
+
+/**
+ * 输入demo
+ * 4
+ * 5
+ * abcd
+ * ab
+ * ad
+ * bc
+ * bd
+ * cd
+ */
 int main() {
 
     Gragh G;
@@ -105,6 +222,13 @@ int main() {
 
     //输出图
     showGraph(G);
+
+    //BFS遍历
+    BFS(G);
+
+    //DFS遍历
+    printf("DFS遍历依次为: ");
+    DFS(G,'a');
 
     return 0;
 
